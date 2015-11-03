b63b6f9db25e361d7a456050b26d23894a1b14a7
chrissl8 <chrisjslaight@gmail.com>
2015-11-03 15:27:04 +0000

    Implement http_basic_auth for Persons, improve styling of various elements

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,4 +1,7 @@
 class PeopleController < ApplicationController
+  
+  http_basic_authenticate_with name: "admin", password: "admin", except: [:index, :show]
+  
   #Index controller to list all people
   def index
     @people = Person.all
