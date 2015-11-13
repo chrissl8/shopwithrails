b40acba895c7cd395e40ec204e0b7a360b593926
chrissl8 <chrisjslaight@gmail.com>
2015-11-13 18:00:05 +0000

    Add 'price' column to gifts as decimal datatype

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151107205527) do
+ActiveRecord::Schema.define(version: 20151113174800) do
 
   create_table "gifts", force: :cascade do |t|
     t.string   "item"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20151107205527) do
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
     t.integer  "purchased",  default: 0
+    t.decimal  "price"
   end
 
   add_index "gifts", ["person_id"], name: "index_gifts_on_person_id"
