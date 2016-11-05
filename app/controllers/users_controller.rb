e4c108389beab2d21851bcfdbd4ce6d2033e2705
chrissl8 <chrisjslaight@gmail.com>
2016-11-05 05:39:38 +0000

    Resolved issue #46, Creating a new user incorrectly re-directs to the login screen

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,8 +26,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
       if @user.save
-        session[:user_id] = @user.id
-        redirect_to root_path
+        ##session[:user_id] = @user.id
+        redirect_to admin_path
       else
         render 'new'
       end
