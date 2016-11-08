8413059e3f23dd527ad5c512f781a740ee0e30e0
chrissl8 <chrisjslaight@gmail.com>
2016-11-08 02:21:46 +0000

    Complete Re-Style List Items, Issue  #57

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,10 +19,14 @@ $(document).on('ready page:load', function() {
 	
 	$('#toggle-open').on('click tap', function() {
 	  $('.gift-extra-data').slideDown(100);
+	  $('#toggle-open').addClass('disabled');
+	  $('#toggle-closed').removeClass('disabled');
 	});
 	
 	$('#toggle-closed').on('click tap', function() {
 	  $('.gift-extra-data').slideUp(100);
+	  $('#toggle-closed').addClass('disabled');
+	  $('#toggle-open').removeClass('disabled');
 	});
 	
 	$('.gift-list-name').on('click tap', function(){
@@ -32,27 +36,40 @@ $(document).on('ready page:load', function() {
 	$('#toggle-all').on('click tap', function() {
 	  $('.remaining-pane').hide();
 	  $('.export-default').show();
+	  $('#toggle-all').addClass('disabled');
+	  $('#toggle-remaining').removeClass('disabled');
 	});
 	
 	$('#toggle-remaining').on('click tap', function() {
 	  $('.export-default').hide();
 	  $('.remaining-pane').show();
+	  $('#toggle-all').removeClass('disabled');
+	  $('#toggle-remaining').addClass('disabled');
 	});
 	
 	//Added to help show purchased, non-purchased or all items
 	$('#toggle-show-all').on('click tap', function() {
 	  $('.gift-list-item-purchased').show();
 	  $('.gift-list-item-not-purchased').show();
+	  $('#toggle-show-np').removeClass('disabled');
+	  $('#toggle-show-p').removeClass('disabled');
+	  $('#toggle-show-all').addClass('disabled');
 	});
 	
 	$('#toggle-show-p').on('click tap', function() {
 	  $('.gift-list-item-purchased').show();
 	  $('.gift-list-item-not-purchased').hide();
+	  $('#toggle-show-np').removeClass('disabled');
+	  $('#toggle-show-all').removeClass('disabled');
+	  $('#toggle-show-p').addClass('disabled');
 	});
 	
 	$('#toggle-show-np').on('click tap', function() {
 	  $('.gift-list-item-purchased').hide();
 	  $('.gift-list-item-not-purchased').show();
+	  $('#toggle-show-p').removeClass('disabled');
+	  $('#toggle-show-all').removeClass('disabled');
+	  $('#toggle-show-np').addClass('disabled');
 	});
 
 });
